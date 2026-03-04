ec9662aa954cd7ca07890f700b2e5e20b33bb7ed
sarker00 <tonmoysarkarfeni@gmail.com>
2026-03-04 10:26:47 -0800

    task2

---
+++ TASK2/task2.js
@@ -0,0 +1,9 @@
+let wrong = 0
+function checkFrom(){
+    let email = document.getElementById("email").value;
+        let pasword = document.getElementById("password").value;
+
+
+
+}
+return false;
